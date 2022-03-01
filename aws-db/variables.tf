f27edffd2cbdfbc6a389e7b2244b1eeea48d5dbb
SantiagoSR <santiago.santa.ramirez@gmail.com>
2022-03-01 10:38:58 -0500

    adding data base module

---
+++ aws-db/variables.tf
@@ -0,0 +1,17 @@
+variable "db_subnet_terraform_name" {
+  description = "Responsible for the creation of the resources"
+  type        = string
+}
+variable "security_groups" {
+  description = "Responsible for the creation of the resources"
+  type        = string
+}
+
+variable "instance_project" {
+  description = "Name of the project"
+  type        = string
+}
+variable "instance_responsile" {
+  description = "Responsible for the creation of the resources"
+  type        = string
+}
