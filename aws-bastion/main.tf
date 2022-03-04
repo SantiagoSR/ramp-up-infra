6387daac1707e9fdb2c314b76217eb7035995367
SantiagoSR <santiago.santa.ramirez@gmail.com>
2022-03-04 11:44:04 -0500

    edit bastion provisioner

--- aws-bastion/main.tf
+++ aws-bastion/main.tf
@@ -28,68 +28,68 @@ resource "aws_instance" "bastion-host" {
 }
 
 
-resource "null_resource" "connection_bastion" {
-  connection {
-    type     = "ssh"
-    user     = "ubuntu"
-    #private_key = file("/home/slow-time/RampUp-devops-santiagosantacruzr.pem") 
-    private_key = file("/vagrant/RampUp-devops-santiagosantacruzr.pem") 
-    host     = aws_instance.bastion-host.public_ip
-  }
-  provisioner "remote-exec" {
-    inline = [
-      "echo Connection is stablished",
-      "cd /home/ubuntu/",
-      "sudo mkdir ansible_repo",
-    ]
-  }
-  depends_on = [ aws_instance.bastion-host ]
-}
-resource "null_resource" "files_to_bastion" {
-  connection {
-    type     = "ssh"
-    user     = "ubuntu"
-    private_key = file("/vagrant/RampUp-devops-santiagosantacruzr.pem") 
-    host     = aws_instance.bastion-host.public_ip
-  }
+# resource "null_resource" "connection_bastion" {
+#   connection {
+#     type     = "ssh"
+#     user     = "ubuntu"
+#     #private_key = file("/home/slow-time/RampUp-devops-santiagosantacruzr.pem") 
+#     private_key = file("/vagrant/RampUp-devops-santiagosantacruzr.pem") 
+#     host     = aws_instance.bastion-host.public_ip
+#   }
+#   provisioner "remote-exec" {
+#     inline = [
+#       "echo Connection is stablished",
+#       "cd /home/ubuntu/",
+#       "sudo mkdir ansible_repo",
+#     ]
+#   }
+#   depends_on = [ aws_instance.bastion-host ]
+# }
+# resource "null_resource" "files_to_bastion" {
+#   connection {
+#     type     = "ssh"
+#     user     = "ubuntu"
+#     private_key = file("/vagrant/RampUp-devops-santiagosantacruzr.pem") 
+#     host     = aws_instance.bastion-host.public_ip
+#   }
 
-  # provisioner "file" {
-  #   source = "/mnt/c/Perficient/RampUp/Iac/new_user_credentials.csv"
-  #   destination = "/home/ubuntu/new_user_credentials.csv"
-  # } 
+#   # provisioner "file" {
+#   #   source = "/mnt/c/Perficient/RampUp/Iac/new_user_credentials.csv"
+#   #   destination = "/home/ubuntu/new_user_credentials.csv"
+#   # } 
 
-  provisioner "file" {
-    #source = "/mnt/c/Perficient/RampUp/.env"
-    source = "/vagrant/.env"
-    destination = "/tmp/.env"
-  }
+#   provisioner "file" {
+#     #source = "/mnt/c/Perficient/RampUp/.env"
+#     source = "/vagrant/.env"
+#     destination = "/tmp/.env"
+#   }
 
-  provisioner "file" {
-    source = "/vagrant/ansible_repo"
-    destination = "/tmp/ansible_repo"
-  }
+#   provisioner "file" {
+#     source = "/vagrant/ansible_repo"
+#     destination = "/tmp/ansible_repo"
+#   }
 
-  depends_on = [ 
-    aws_instance.bastion-host,
-    null_resource.connection_bastion
+#   depends_on = [ 
+#     aws_instance.bastion-host,
+#     null_resource.connection_bastion
 
-   ]
-}
-resource "null_resource" "move_env" {
-  connection {
-    type     = "ssh"
-    user     = "ubuntu"
-    private_key = file("/vagrant/RampUp-devops-santiagosantacruzr.pem") 
-    host     = aws_instance.bastion-host.public_ip
-  }
-  provisioner "remote-exec" {
-    inline = [
-      "cd /home/ubuntu/ansible_repo",
-      "sudo mv /tmp/ansible_repo/* /home/ubuntu/ansible_repo/",
-      "sudo mv /tmp/.env /home/ubuntu/ansible_repo/roles/web-servers/files",
-      "sudo chown -R ubuntu:ubuntu /home/ubuntu/ansible_repo/",
-    ]
-  }
-  depends_on = [ null_resource.files_to_bastion ]
-}
+#    ]
+# }
+# resource "null_resource" "move_env" {
+#   connection {
+#     type     = "ssh"
+#     user     = "ubuntu"
+#     private_key = file("/vagrant/RampUp-devops-santiagosantacruzr.pem") 
+#     host     = aws_instance.bastion-host.public_ip
+#   }
+#   provisioner "remote-exec" {
+#     inline = [
+#       "cd /home/ubuntu/ansible_repo",
+#       "sudo mv /tmp/ansible_repo/* /home/ubuntu/ansible_repo/",
+#       "sudo mv /tmp/.env /home/ubuntu/ansible_repo/roles/web-servers/files",
+#       "sudo chown -R ubuntu:ubuntu /home/ubuntu/ansible_repo/",
+#     ]
+#   }
+#   depends_on = [ null_resource.files_to_bastion ]
+# }
 
